1e04ead3bfc0012a7a45107e9654f77129368e2b
CodeQuiver <38484031+CodeQuiver@users.noreply.github.com>
2018-07-10 20:46:15 -0400

    write new input to database setup

--- scripts.js
+++ scripts.js
@@ -1,15 +1,57 @@
 // NextTrain train-timer app
 
+// Initialize Firebase
+    // This is the code we copied and pasted from our app page
+    var config = {
+        apiKey: "AIzaSyAJS4YQWU5DmESeYueG1qH1NGkjv3DncEY",
+        authDomain: "fir-click-counter-7cdb9.firebaseapp.com",
+        databaseURL: "https://fir-click-counter-7cdb9.firebaseio.com",
+        storageBucket: "fir-click-counter-7cdb9.appspot.com"
+      };
+  
+      firebase.initializeApp(config);
+  
 
 //===================== GLOBAL VARIABLES ========================//
+
+    // Get a reference to the database service
+    var dataRef = firebase.database();
+
+
 //===================== END GLOBAL VARIABLES ========================//
 
 //===================== FUNCTIONS ========================//
     // NEW TRAIN FUNCTION
-        // on form submit
-        // captures form data in variables
-        // packages form data
-        // pushes to firebase DB
+        // on click of submit button #add-train
+        
+    $("#add-train").on("click", function(event) {
+        event.preventDefault();
+
+        // initialize local variables
+        name = "";
+        destination = "";
+        start = "";
+        frequency = "";
+
+        // capture form data in variables
+        name = $("#new-train-name").val().trim();
+        destination = $("#train-destination").val().trim();
+        start = $("#train-start-time").val().trim();
+        frequency = $("#train-frequency").val().trim();
+
+        // package form data
+        // pushes to Firebase in a JSON property
+        // Note how we are using the Firebase .push() method to add a new object without overwriting the others
+        dataRef.ref().push({
+        
+        name: name,
+        destination: destination,
+        start: start,
+        frequency: frequency,
+        dateAdded: firebase.database.ServerValue.TIMESTAMP
+      });
+    });
+
     // END NEW TRAIN FUNCTION
 
     // FIREBASE FETCHING FUNCTION
