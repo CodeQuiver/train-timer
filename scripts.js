2706bcd87da180d333c292ad88f30ef3926e2d31
CodeQuiver <38484031+CodeQuiver@users.noreply.github.com>
2018-07-11 13:59:08 -0400

    set up train time calculations

--- scripts.js
+++ scripts.js
@@ -30,19 +30,33 @@ firebase.initializeApp(config);
     }
     // END CLEAR FORM FUNCTION
 
-    // CALC NEXT TRAIN FUNCTION
-    function calcNextTrain(calcStart, calcFreq) {
-        // moment.js code here
-    }
+    // CALC NEXT TRAIN FUNCTION combined with CALC MINUTES TO ARRIVAL
         // receives the train's start time and frequency as arguments
         // calculates next train time using moment.js
-    // END CALC NEXT TRAIN FUNCTION
 
-    // CALC MINUTES TO ARRIVAL
-    function calcMinutes(calcStart, calcFreq) {
-        
+    function calcNextTrain(calcStart, calcFreq) {
+        // First Time (pushed back 1 year to make sure it comes before current time)
+        var startTimeConverted = moment(calcStart, "HH:mm").subtract(1, "years");
+
+        // Difference between the times
+        var diffTime = moment().diff(moment(startTimeConverted), "minutes");
+        console.log("DIFFERENCE IN TIME: " + diffTime);
+
+        // Time apart (remainder)
+        var tRemainder = diffTime % calcFreq;
+        console.log(tRemainder);
+
+        // Minutes Until Train
+        var tMinutesTillTrain = calcFreq - tRemainder;
+        console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+        // Next Train
+        var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+        console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+
+        return { nextTrain: nextTrain, tMinutesTillTrain: tMinutesTillTrain };
     }
-    // END CALC MINUTES TO ARRIVAL
+    // END CALC NEXT TRAIN FUNCTION
 
     // NEW TRAIN FUNCTION
         // on click of submit button #add-train
@@ -92,13 +106,14 @@ firebase.initializeApp(config);
             loopStart = loopTrain.val().start;
             loopFreq = loopTrain.val().frequency;
             
-            // pass argument to and call calculation function for "Next Train Time"
-            // save result in variable
-            loopArrival = calcNextTrain(loopStart, loopFreq);
-            
-            // pass argument to and call calculation function for "Minutes until Arrival"
-            // save result in variable
-            loopMinutes = calcMinutes(loopStart, loopFreq);
+            // pass argument to and call calculation function for "Next Train Time" and "Minutes until Arrival"
+            // save results in variables
+            nextTrainResponse = calcNextTrain(loopStart, loopFreq);
+            loopArrival = nextTrainResponse.nextTrain;
+            loopMinutes = nextTrainResponse.tMinutesTillTrain;
+
+            //console log to check
+            console.log("Next Train Time" + i + ": " + loopArrival + " Minutes until next Train" + i + ": " + loopMinutes);
 
             // call "PRINT ROW" function and pass all arguments
             printTableRow(loopName, loopDest, loopFreq, loopArrival, loopMinutes);
